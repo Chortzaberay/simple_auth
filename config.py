f1b9deb0ff8c5b59737f7a30dd9a040531c44fa0
Shu <stascort1@gmail.com>
2023-01-20 15:35:00 +0200

    beter config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+from constants import DB_PASSWORD, DB_USER
 class Config():
     TESTING = False
 
 class DevConfig(Config):
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:HomePass1@localhost/basic_login"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/basic_login"
     SECRET_KEY = "secret_key"
+    DEBUG = True
 
 class ProductConfig(Config):
     TESTING = False
